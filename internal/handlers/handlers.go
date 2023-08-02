c64d35ca65add90a75052c55d047ea9c91e9fb46
MohammadJavad14 <mrph14@gamil.com>
2023-08-02 22:30:33 +0330

    adding additional socket event handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -34,6 +34,17 @@ type WsJsonResponse struct {
 	MessageType string `json:"message_type"`
 }
 
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
+}
+
 // WsEndpoint upgrades connection to websocket
 func WsEndpoint(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgradeConnection.Upgrade(w, r, nil)
